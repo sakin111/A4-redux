90596944ff014fd68a158eab41a4fc7ccf1b7631
sakin111 <maliksakin53@gmail.com>
2025-07-07 01:21:37 +0600

    .

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     reducerPath: "baseApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "https://a4-server-redux.vercel.app/api/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://a4-server-redux.vercel.app/api" }),
 
     tagTypes: ["books", "borrows"],
     endpoints: (builder) => ({
